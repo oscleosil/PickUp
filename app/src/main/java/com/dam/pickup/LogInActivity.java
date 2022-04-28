f244378b2b71c2aeb2bc3158c16d81f1e91aa28f
oscleosil <leon99silva@gmail.com>
2022-04-28 15:37:44 +0200

    SignInActivity and LogInActivity modified

--- app/src/main/java/com/dam/pickup/LogInActivity.java
+++ app/src/main/java/com/dam/pickup/LogInActivity.java
@@ -13,11 +13,15 @@ import android.widget.Toast;
 import android.content.Intent;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.android.gms.tasks.*;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -62,10 +66,10 @@ public class LogInActivity extends AppCompatActivity {
                                         Toast.makeText(getApplicationContext(),
                                                 "Usuario registrado correctamente",
                                                 Toast.LENGTH_LONG).show();
-                                        /*SharedPreferences.Editor prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit();
+                                        SharedPreferences.Editor prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit();
                                         prefs.putString("email", email_txt);
                                         prefs.putString("provider", provider.BASIC.toString());
-                                        prefs.apply(); ToDo*/
+                                        prefs.apply();
                                         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                                         startActivity(intent);
                                     } else {
@@ -100,22 +104,54 @@ public class LogInActivity extends AppCompatActivity {
             }
         });
 
-        /*log_in_with_google.setOnClickListener(new View.OnClickListener() {
+        log_in_with_google.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                 final GoogleSignInOptions google_conf = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                 GoogleSignInOptions google_conf = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                          .requestIdToken(getString(R.string.default_web_client_id))
                          .requestEmail()
                          .build();
-                final GoogleSignInClient google_client = GoogleSignIn.getClient(getApplicationContext(), google_conf);
-                startActivityForResult(google_client.getSignInIntent(), GOOGLE_SIGN_IN);
+                 GoogleSignInClient google_client = GoogleSignIn.getClient(getApplicationContext(), google_conf);
+                 google_client.signOut();
+                 startActivityForResult(google_client.getSignInIntent(), GOOGLE_SIGN_IN);
             }
-        });*/
+        });
     }
 
-    /*@Override
-    public void onActivityResult(int request_code, int result_code, Intent intent) {
-        super.onActivityResult(request_code, result_code, intent);
-        //ToDo
-    }*/
+    @Override
+    public void onActivityResult(int request_code, int result_code, Intent data) {
+        super.onActivityResult(request_code, result_code, data);
+        if(request_code== GOOGLE_SIGN_IN){
+            Task<GoogleSignInAccount> sign_in= GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = sign_in.getResult(ApiException.class);
+                if(account != null){
+                    AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Usuario registrado correctamente",
+                                        Toast.LENGTH_LONG).show();
+                                        SharedPreferences.Editor prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit();
+                                        prefs.putString("email", account.getEmail());
+                                        prefs.putString("provider", provider.GOOGLE.toString());
+                                        prefs.apply();
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Se ha producido un error, por favor, intentelo de nuevo",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                    });;
+                }
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- app/src/main/java/com/dam/pickup/SignInActivity.java
+++ app/src/main/java/com/dam/pickup/SignInActivity.java
@@ -10,17 +10,25 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.content.Intent;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.android.gms.tasks.*;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignInActivity extends AppCompatActivity {
 
     private EditText email;
     private EditText pass;
     private Button sign_in;
-    private Button sign_in_google;
+    private Button sign_in_with_google;
+    private enum provider{BASIC, GOOGLE};
+    private final int GOOGLE_SIGN_IN = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +37,7 @@ public class SignInActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         pass = findViewById(R.id.password);
         sign_in = findViewById(R.id.signin);
-        sign_in_google = findViewById(R.id.signin_with_google);
+        sign_in_with_google = findViewById(R.id.signin_with_google);
         setUp();
     }
 
@@ -64,13 +72,56 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
-        /*sign_in_google.setOnClickListener(new View.OnClickListener() {
-        ToDo
+
+        sign_in_with_google.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final GoogleSignInOptions.Builder google_opt = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN);
+                GoogleSignInOptions google_conf = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
+                GoogleSignInClient google_client = GoogleSignIn.getClient(getApplicationContext(), google_conf);
+                google_client.signOut();
+                startActivityForResult(google_client.getSignInIntent(), GOOGLE_SIGN_IN);
             }
-        });*/
-
+        });
     }
+
+    @Override
+    public void onActivityResult(int request_code, int result_code, Intent data) {
+        super.onActivityResult(request_code, result_code, data);
+        if(request_code== GOOGLE_SIGN_IN){
+            Task<GoogleSignInAccount> sign_in= GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = sign_in.getResult(ApiException.class);
+                if(account != null){
+                    AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Usuario registrado correctamente",
+                                        Toast.LENGTH_LONG).show();
+                                        /*SharedPreferences.Editor prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit();
+                                        prefs.putString("email", email_txt);
+                                        prefs.putString("provider", provider.BASIC.toString());
+                                        prefs.apply(); ToDo*/
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Se ha producido un error, por favor, intentelo de nuevo",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                    });;
+                }
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
